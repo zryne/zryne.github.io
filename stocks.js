620cefd906fc304c5158faedf9df62af7a2f44bc
Zinnu Ryne <38824337+zryne@users.noreply.github.com>
2021-11-22 12:17:24 -0500

    Added more code, but not fruitful yet

--- stocks.js
+++ stocks.js
@@ -1,11 +1,26 @@
-const BtnAdd = document.querySelector(".btn-add");
+//const BtnAdd = document.querySelector(".btn-add");
+
 const DivContainer = document.getElementById("div-container");
 
-BtnAdd.addEventListener("click", AddNew);
+//BtnAdd.addEventListener("click", showAlert);
+//BtnAdd.addEventListener("click", AddNew);
 
 function AddNew() {
   const newDiv = document.createElement("div");
   console.log("add");
   newDiv.classList.add("div-shadow");
   DivContainer.appendChild(newDiv);
+}
+
+function showAlert() {
+  alert("Hello")
+}
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
